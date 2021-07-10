a53743ff0912d47325cfe98a322091c79f1f606a
Gideon Shaked <gideonshaked@gmail.com>
2021-07-10 10:49:43 -0400

    Fix typo bug and remove zero cmd

--- src/client/gui.py
+++ src/client/gui.py
@@ -17,10 +17,6 @@ def update():
     send_data.send({"x": x, "y": y, "z": z, "a": a, "b": b})
 
 
-def zero():
-    send_data.send("")
-
-
 # Axis Value tkinter Variables
 x_target = tk.DoubleVar(value=0)
 y_target = tk.DoubleVar(value=0)
@@ -29,24 +25,21 @@ a_target = tk.DoubleVar(value=0)
 b_target = tk.DoubleVar(value=0)
 
 # Axis Labels
-x_label = tk.Label(root, text="X Axis 0-50", font=("calibre", 10, "bold"))
-y_label = tk.Label(root, text="Y Axis 0-50", font=("calibre", 10, "bold"))
-z_label = tk.Label(root, text="Z axis 0-50", font=("calibre", 10, "bold"))
-a_label = tk.Label(root, text="A axis 0-50", font=("calibre", 10, "bold"))
-b_label = tk.Label(root, text="B axis 0-50", font=("calibre", 10, "bold"))
+x_label = tk.Label(root, text="X Axis 0-50mm", font=("calibre", 10, "bold"))
+y_label = tk.Label(root, text="Y Axis 0-50mm", font=("calibre", 10, "bold"))
+z_label = tk.Label(root, text="Z axis 0-50mm", font=("calibre", 10, "bold"))
+a_label = tk.Label(root, text="A axis 0-90°", font=("calibre", 10, "bold"))
+b_label = tk.Label(root, text="B axis 0-50mm", font=("calibre", 10, "bold"))
 
 # Axis Value Entry Widget
 x_entry = tk.Entry(root, textvariable=x_target, font=("calibre", 10, "normal"))
 y_entry = tk.Entry(root, textvariable=y_target, font=("calibre", 10, "normal"))
 z_entry = tk.Entry(root, textvariable=z_target, font=("calibre", 10, "normal"))
-a_target = tk.Entry(root, textvariable=a_target, font=("calibre", 10, "normal"))
+a_entry = tk.Entry(root, textvariable=a_target, font=("calibre", 10, "normal"))
 b_entry = tk.Entry(root, textvariable=b_target, font=("calibre", 10, "normal"))
 
 # Update Values on Server Button Widget
 update_button = tk.Button(root, text="Update", command=update)
-
-# Zero All Axes Button
-zero_button = tk.Button(root, text="Zero All Axes", command=zero)
 
 # Positions of Widgets
 x_label.grid(row=0, column=0)
@@ -59,13 +52,11 @@ z_label.grid(row=2, column=0)
 z_entry.grid(row=2, column=1)
 
 a_label.grid(row=3, column=0)
-a_target.grid(row=3, column=1)
+a_entry.grid(row=3, column=1)
 
 b_label.grid(row=4, column=0)
 b_entry.grid(row=4, column=1)
 
 update_button.grid(row=5, column=1)
-
-zero_button.grid(row=5, column=0)
 
 root.mainloop()
