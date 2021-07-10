0ee09774bdea80506ff8241c4ba82f9656889ee1
Gideon Shaked <gideonshaked@gmail.com>
2021-07-10 10:49:14 -0400

    Add external control of log level

--- src/server/server/__main__.py
+++ src/server/server/__main__.py
@@ -1,12 +1,20 @@
 import atexit
 import logging
+import os
 from threading import Thread
 
 from . import movement_controller, receive_data
 
+log_levels = {"INFO": logging.INFO, "DEBUG": logging.DEBUG}
+
+log_level = log_levels[os.getenv("LOG_LEVEL", default="DEBUG")]
+
 logging.basicConfig(
-    format="{asctime} {levelname}: {message}", style="{", level=logging.INFO, datefmt="%m/%d/%Y %I:%M:%S %p"
+    format="{asctime} {levelname}: {message}", style="{", level=log_level, datefmt="%m/%d/%Y %I:%M:%S %p"
 )
+
+logging.info(f"Log level: {log_level}")
+logging.info(f"Server version: {__import__('server').__version__}")
 
 atexit.register(movement_controller.clean_up)
 
