c93670abc084d0b5d4c0ed78224cf4af98ddee97
Gideon Shaked <gideonshaked@gmail.com>
2021-07-22 21:51:06 -1000

    Add several quality of life features
    input verification
    +/- 25 / +/- 45
    Error dialogue boxes
    Better labeling / justification of text

--- src/client/gui.py
+++ src/client/gui.py
@@ -4,21 +4,53 @@ import send_data
 
 root = tk.Tk()
 root.title("PALMS v0.4.1")
-root.geometry("600x400")
+root.geometry("")
 
 current_data = {"x": 0.0, "y": 0.0, "z": 0.0, "a": 0.0, "b": 0.0}
 
+FONT_SIZE = 14
 
-def update():
-    """Update target values on server."""
+
+def update() -> None:
+    """Update target values on server"""
     new_data = {"x": x_target.get(), "y": y_target.get(), "z": z_target.get(), "a": a_target.get(), "b": b_target.get()}
+
     global current_data
-    send_data.send(changed_values(new_data, current_data))
-    current_data = new_data
+
+    if verify_targets(new_data):
+        try:
+            send_data.send(change_position_scale(changed_values(new_data, current_data)))
+        except send_data.ConnectionError as e:
+            dialogue_box("Error Updating Targets", e.message)
+        else:
+            current_data = new_data
+    else:
+        dialogue_box("Invalid Position(s)", "One or more entered positions are invalid.")
+
+
+def dialogue_box(title, message) -> None:
+    dialogue_box = tk.Toplevel(root)
+    dialogue_box.geometry("")
+    dialogue_box.resizable(False, False)
+    dialogue_box.title(title)
+    tk.Label(dialogue_box, text=message, font=("calibre", 12, "normal")).pack(expand=1)
 
 
 def changed_values(new_data: dict, current_data: dict) -> dict:
     return {key: new_data[key] for key, current_value in current_data.items() if new_data[key] != current_value}
+
+
+def verify_targets(targets: dict) -> bool:
+    for key, val in targets.items():
+        if key == "a" and (val > 45 or val < -45):
+            return False
+        elif val > 25 or val < -25:
+            return False
+    return True
+
+
+def change_position_scale(targets: dict) -> dict:
+    return {key: val + 45 if key == "a" else val + 25 for key, val in targets.items()}
 
 
 # Axis Value tkinter Variables
@@ -29,21 +61,25 @@ a_target = tk.DoubleVar(value=0)
 b_target = tk.DoubleVar(value=0)
 
 # Axis Labels
-x_label = tk.Label(root, text="X Axis 0-50mm", font=("calibre", 10, "bold"))
-y_label = tk.Label(root, text="Y Axis 0-50mm", font=("calibre", 10, "bold"))
-z_label = tk.Label(root, text="Z axis 0-50mm", font=("calibre", 10, "bold"))
-a_label = tk.Label(root, text="A axis 0-90°", font=("calibre", 10, "bold"))
-b_label = tk.Label(root, text="B axis 0-50mm", font=("calibre", 10, "bold"))
+label_kwargs = {"width": 17, "anchor": tk.W, "font": ("calibre", FONT_SIZE, "bold")}
+
+x_label = tk.Label(root, text="X ±25mm", **label_kwargs)
+y_label = tk.Label(root, text="Y ±25mm", **label_kwargs)
+z_label = tk.Label(root, text="Z ±25mm", **label_kwargs)
+a_label = tk.Label(root, text="Rotational ±45°", **label_kwargs)
+b_label = tk.Label(root, text="Collimator ±25mm", **label_kwargs)
 
 # Axis Value Entry Widget
-x_entry = tk.Entry(root, textvariable=x_target, font=("calibre", 10, "normal"))
-y_entry = tk.Entry(root, textvariable=y_target, font=("calibre", 10, "normal"))
-z_entry = tk.Entry(root, textvariable=z_target, font=("calibre", 10, "normal"))
-a_entry = tk.Entry(root, textvariable=a_target, font=("calibre", 10, "normal"))
-b_entry = tk.Entry(root, textvariable=b_target, font=("calibre", 10, "normal"))
+entry_kwargs = {"font": ("calibre", FONT_SIZE, "normal")}
+
+x_entry = tk.Entry(root, textvariable=x_target, **entry_kwargs)
+y_entry = tk.Entry(root, textvariable=y_target, **entry_kwargs)
+z_entry = tk.Entry(root, textvariable=z_target, **entry_kwargs)
+a_entry = tk.Entry(root, textvariable=a_target, **entry_kwargs)
+b_entry = tk.Entry(root, textvariable=b_target, **entry_kwargs)
 
 # Update Values on Server Button Widget
-update_button = tk.Button(root, text="Update", command=update)
+update_button = tk.Button(root, text="Update", command=update, font=("calibre", FONT_SIZE, "normal"))
 
 # Positions of Widgets
 x_label.grid(row=0, column=0)
