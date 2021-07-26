d669faca0ca76ef37e728df2a0804f55905f4d14
Gideon Shaked <gideonshaked@gmail.com>
2021-07-26 11:16:15 -1000

    Go back to +/- 50

--- src/client/client/gui.py
+++ src/client/client/gui.py
@@ -25,35 +25,34 @@ class MainApplication:
         self.make_buttons()
 
         self.settings = Settings()
-        self.settings.add_setting("Legacy Position Indicator", False)
-
-        if self.settings["Legacy Position Indicator"]:
-            self.enable_legacy_position_indicator()
 
         self.frame.pack()
 
         self.current_data = {"x": 0.0, "y": 0.0, "z": 0.0, "a": 0.0, "b": 0.0}
 
     def make_axis_widgets(self) -> None:
-        self.x_position, self.x_label, self.x_entry = self.axis_widgets("X ±25mm", 0)
-        self.y_position, self.y_label, self.y_entry = self.axis_widgets("Y ±25mm", 1)
-        self.z_position, self.z_label, self.z_entry = self.axis_widgets("Z ±25mm", 2)
-        self.a_position, self.a_label, self.a_entry = self.axis_widgets("Rotational ±45°", 3)
-        self.b_position, self.b_label, self.b_entry = self.axis_widgets("Collimator ±25mm", 4)
+        self.x_position, self.x_label, self.x_entry = self.axis_widgets("X 0-50mm", 0)
+        self.y_position, self.y_label, self.y_entry = self.axis_widgets("Y 0-50mm", 1)
+        self.z_position, self.z_label, self.z_entry = self.axis_widgets("Z 0-50mm", 2)
+        self.a_position, self.a_label, self.a_entry = self.axis_widgets("Rotational 0-90°", 3)
+        self.b_position, self.b_label, self.b_entry = self.axis_widgets("Collimator 0-50mm", 4)
 
     def make_buttons(self) -> None:
         self.update_button = tk.Button(self.frame, text="Update", command=self.update, font=FONT).grid(
             row=5, column=1, padx=10, pady=10, sticky="nesw"
         )
         self.lock_button = tk.Button(
-            self.frame, text="Lock (placeholder)", command=print, font=FONT
+            self.frame,
+            text="Lock (placeholder)",
+            #            command=print,
+            font=FONT,
         ).grid(  # TODO: Add lock functionality
             row=5, column=0, padx=10, pady=10, sticky="nesw"
         )
         self.settings_button = tk.Button(
             self.frame,
             text="Settings",
-            command=lambda: SettingsWindow(tk.Toplevel(self.master), self.settings),
+            #            command=lambda: SettingsWindow(tk.Toplevel(self.master), self.settings),
             font=FONT,
         ).grid(row=6, column=0, columnspan=2, padx=10, pady=10, sticky="nesw")
 
@@ -72,27 +71,6 @@ class MainApplication:
     def position_entry(self, textvar: tk.DoubleVar) -> tk.Entry:
         return tk.Entry(self.frame, textvariable=textvar, font=FONT)
 
-    def enable_legacy_position_indicator(self) -> None:
-        self.x_lpi_label = tk.Label(self.frame, text="X: 25", font=FONT, padx=10)
-        self.y_lpi_label = tk.Label(self.frame, text="Y: 25", font=FONT, padx=10)
-        self.z_lpi_label = tk.Label(self.frame, text="Z: 25", font=FONT, padx=10)
-        self.a_lpi_label = tk.Label(self.frame, text="A: 25", font=FONT, padx=10)
-        self.b_lpi_label = tk.Label(self.frame, text="B: 25", font=FONT, padx=10)
-
-        self.x_lpi_label.grid(row=0, column=3, sticky="nesw")
-        self.y_lpi_label.grid(row=1, column=3, sticky="nesw")
-        self.z_lpi_label.grid(row=2, column=3, sticky="nesw")
-        self.a_lpi_label.grid(row=3, column=3, sticky="nesw")
-        self.b_lpi_label.grid(row=4, column=3, sticky="nesw")
-
-    def update_legacy_position_indicator_labels(self, positions: dict) -> None:
-        scaled_positions = self.change_position_scale(positions)
-        self.x_lpi_label.config(text=f"X: {scaled_positions['x']}")
-        self.y_lpi_label.config(text=f"Y: {scaled_positions['y']}")
-        self.z_lpi_label.config(text=f"Z: {scaled_positions['z']}")
-        self.a_lpi_label.config(text=f"A: {scaled_positions['a']}")
-        self.b_lpi_label.config(text=f"B: {scaled_positions['b']}")
-
     def update(self) -> None:
         """Update target values on server"""
         new_data = {
@@ -103,11 +81,10 @@ class MainApplication:
             "b": self.b_position.get(),
         }
 
-        self.update_legacy_position_indicator_labels(new_data)
-
         if self.verify_positions(new_data):
             try:
-                send_data.send(self.change_position_scale(self.changed_values(new_data, self.current_data)))
+                print(self.changed_values(new_data, self.current_data))
+                send_data.send(self.changed_values(new_data, self.current_data))
             except send_data.ServerConnectionError as e:
                 DialogBox(tk.Toplevel(self.master), "Error Updating Targets", e.message)
             else:
@@ -122,15 +99,11 @@ class MainApplication:
     @staticmethod
     def verify_positions(positions: dict) -> bool:
         for key, val in positions.items():
-            if key == "a" and (val > 45 or val < -45):
+            if key == "a" and (val > 90 or val < 0):
                 return False
-            elif val > 25 or val < -25:
+            if val > 50 or val < 0:
                 return False
         return True
-
-    @staticmethod
-    def change_position_scale(positions: dict) -> dict:
-        return {key: val + 45 if key == "a" else val + 25 for key, val in positions.items()}
 
     def report_callback_exception(self, *args):
         DialogBox(
@@ -187,8 +160,6 @@ class SettingsWindow:
         self.frame.grab_set()
 
         self.settings = settings
-
-        self.legacy_position_indicator_widgets = self.setting_widgets("Legacy Position Indicator", 0)
 
         self.frame.pack()
 
